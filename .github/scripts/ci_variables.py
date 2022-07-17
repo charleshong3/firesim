18b44f813e3de778fc17428ff854426d9ad007ad
Abraham Gonzalez <abe.j.gonza@gmail.com>
2022-07-17 18:49:04 +0000

    Unique runners for workflow run | Delete offline runners

--- .github/scripts/ci_variables.py
+++ .github/scripts/ci_variables.py
@@ -7,6 +7,7 @@ from local_flags import RUN_LOCAL, local_fsim_dir
 # CI instance environment variables
 # This is used as a unique tag for all instances launched in a workflow
 ci_workflow_run_id = os.environ['GITHUB_RUN_ID'] if not RUN_LOCAL else 0
+ci_workflow_run_retries = os.environ['GITHUB_RUN_ATTEMPT'] if not RUN_LOCAL else 0
 ci_commit_sha1 = os.environ['GITHUB_SHA'] if not RUN_LOCAL else 0
 # expanduser to replace the ~ present in the default, for portability
 ci_workdir = os.path.expanduser(os.environ['GITHUB_WORKSPACE']) if not RUN_LOCAL else local_fsim_dir
